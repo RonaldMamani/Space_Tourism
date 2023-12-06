774c166207b5d3c7ba1b8bfa1fb21776c1cdb6b7
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-12-06 19:43:56 -0300

    Terminando a seção Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,25 @@
+import { Link } from "react-router-dom"
 import { Header } from "../components/Header"
 
 export default function Home() {
     return (
-        <div className="bg-[url('./assets/home/background-home-desktop.jpg')] bg-cover h-screen">
+        <div className="bg-[url('./assets/home/background-home-desktop.jpg')] bg-center bg-cover h-screen">
             <Header />
             <section className="
                 px-4 py-4
                 md:p-10
-                lg:px-40 lg:py-32 lg:grid lg:grid-cols-2 lg:gap-40 items-center
+                lg:px-52 lg:pt-48 lg:flex lg:justify-between lg:items-end
                 ">
-                <div className="flex flex-col gap-10 w-[470px] text-white">
+                <div className="flex flex-col gap-5 w-[470px] text-gray-300">
                     <h4 className="text-3xl">SO, YOU WANT TO TRAVEL TO</h4>
-                    <h3 className="text-9xl font-semibold text-center uppercase">Space</h3>
+                    <h3 className="text-[10rem] font-semibold uppercase text-white">Space</h3>
                     <p className="text-lg">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
-                <button className="w-[300px] h-[300px] flex-shrink bg-white rounded-full z-10">
-                    <p className="text-4xl font-light uppercase">Explore</p>
-                </button>
+                <Link to="/destination" className="pr-20">
+                    <button className="w-[300px] h-[300px] flex-shrink bg-white rounded-full">
+                        <p className="text-4xl uppercase">Explore</p>
+                    </button>
+                </Link>
             </section>
         </div>
     )
